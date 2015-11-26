dd24506b6510d28c1e16e5812d4f60e42b481e42
hanmeimei222 <978288958@qq.com>
2015-11-27 00:14:12 +0800

    test for chinese

--- ex1.py
+++ ex1.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 print "Hello World!"
-#print "Hello Again"
-#print "I like typing this."
-#print "This is fun."
-#print 'Yay! Printing.'
-#print "I'd much rather you 'not'."
-#print 'I "said" do not touch this.'
-#print "print one more line"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "print one more line"
 #print "test for '#'"
+print "test for 中文"
