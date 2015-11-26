02100596d62d66a6b8d92f1cc7d4bc8823e6b5a0
hanmeimei222 <978288958@qq.com>
2015-11-27 07:19:48 +0800

    this is drill of ex3

--- ex3.py
+++ ex3.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 print "I will now count my chickens"
-print "testttt"
+
 # 练习整型的+ - * / %
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+print "Hens", 25 + 30.0 / 6.0
+print "Roosters", 100 - 25.0 * 3.0 % 4.0
 
 print "Now I will count the eggs:"
 
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3 + 2 + 1 - 5 + 4.0 % 2.0 - 1.0 / 4.0 + 6
 
 print "Is it true that 3 + 2 < 5 - 7?"
 
