6cbc8a59f80ce7b2bed31558bcf61818d792e49e
hanmeimei222 <978288958@qq.com>
2015-11-29 00:04:45 +0800

    add some supplement for ex5.py on drills

--- ex5.py
+++ ex5.py
@@ -4,18 +4,24 @@ name = 'Shaw'
 age = 35  # not a lie
 height = 74  # inches
 weight = 180  # lbs
-eyes = 'Blue'
+eyes = "Blue"
 teeth = 'White'
 hair = 'Brown'
+num1 = 0.000003333333
+num2 = 0.3333
 
 # exercise for "format string"
 print "Let's talk about %s." % name
-# 十进制 %d
+# 十进制整型 %d 或 %i
 print "He's %d years old." % age
-# 十六进制 %x
+# 无符号整型 %u
+print "He's %u years old." % 0xEF3
+# 十六进制 %x 或 %X
 print "He's %x years old." % age
 # 八进制 %o
 print "He's %o years old." % age
+# 字符及ASCII码 %c
+print "He's grade is %c." % 'a'
 # 格式化输出浮点数
 print "He's %f years old." % age
 # 格式化输出浮点数（制定保留小数点位数）
@@ -26,8 +32,11 @@ print "name:%10s, age:%8d, height:%8.2f" % (name, age, height)
 print "name:%-10s, age:%-8d, height:%-8.2f" % (name, age, height)
 # 制定占位符（0）
 print "name:%-10s, age:%08d, height:%08.2f" % (name, age, height)
-# 科学计数法
+# 科学计数法 %e 或 %E
 print "height:%.2e" % height
+# %f 和‘%e 默认情况下都会保留小数点后面六位有效数字 %g或%G 在保证六位有效数字的前提下，使用小数方式，否则使用科学计数法
+print "%f, %e, %g" % (num1, num1, num1)
+print "%f, %e, %g" % (num2, num2, num2)
 # 四舍五入
 print round(1.7333)
 # 英寸->厘米
