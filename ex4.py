090b74945fde093fcc73e7fc9eadb595bcece5b6
hanmeimei222 <978288958@qq.com>
2015-11-28 20:09:20 +0800

    this is ex4.py commit on master

---
+++ ex4.py
@@ -0,0 +1,17 @@
+# define variables and names,use "_" as space between words in variable names
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+average_passengers_per_car = passengers / cars_driven
+
+# print both string and variables and use "," to join both of them
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpooll today."
+print "We need to put about", average_passengers_per_car, "in each car."
