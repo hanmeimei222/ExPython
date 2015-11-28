c1f9fc463221078f847f8ede52f70d856aa64487
hanmeimei222 <978288958@qq.com>
2015-11-28 23:13:33 +0800

    this is drill of ex5.py commit on drills

--- ex5.py
+++ ex5.py
@@ -1,6 +1,6 @@
 # -- coding: utf-8 --
 # use ':%/my_//g' to remove all the prefix 'my_'
-name = 'Zed A. Shaw'
+name = 'Shaw'
 age = 35  # not a lie
 height = 74  # inches
 weight = 180  # lbs
@@ -20,7 +20,14 @@ print "He's %o years old." % age
 print "He's %f years old." % age
 # 格式化输出浮点数（制定保留小数点位数）
 print "He's %.3f years old." % age
-
+# 指定占位符宽度
+print "name:%10s, age:%8d, height:%8.2f" % (name, age, height)
+# 制定占位符宽度（左对其）
+print "name:%-10s, age:%-8d, height:%-8.2f" % (name, age, height)
+# 制定占位符（0）
+print "name:%-10s, age:%08d, height:%08.2f" % (name, age,height)
+# 科学计数法
+print "height:%.2e" % height
 print "He's %d inches tall." % height
 print "He's %d pounds heavy." % weight
 print "Actually that's not too heavy."
