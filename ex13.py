8037d834a8ce1baa7b1c2393a006d401edd6a7d9
hanmeimei222 <978288958@qq.com>
2015-12-01 09:48:13 +0800

    this is ex13.py commit on master for argv

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# unpacks argv,run python like "python ex13.py 1 second 3th"
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
