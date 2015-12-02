999c461f43c79c39af3f8fa39c0518964c204b6c
hanmeimei222 <978288958@qq.com>
2015-12-03 06:46:41 +0800

    This is ex25.py commit on master use python commond to call it and its functions

--- ex24.py
+++ ex24.py
@@ -1,5 +1,6 @@
 print "Let's practice everything."
-print 'You\'d need to know \'about escapes with \\ that do \n newlines and \t tabs.'
+print 'You\'d need to know \'about escapes \
+with \\ that do \n newlines and \t tabs.'
 
 poem = """
 \tThe lovely so firmly planted
@@ -17,6 +18,7 @@ print "------------------"
 five = 10 - 2 + 3 - 6
 print "This should be five: %s" % five
 
+
 def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
@@ -33,4 +35,5 @@ print "We'd have %d beans, %d jars, and %d crates" % (beans, jars, crates)
 start_point = start_point / 10
 
 print "We can also do that this way:"
-print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point) 
+print "We'd have %d beans, %d jars, and %d crates." %\
+    secret_formula(start_point)
