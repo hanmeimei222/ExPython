9cf83396dd6bb0da4b9c8fc8fd3c94fb3873b36e
hanmeimei222 <978288958@qq.com>
2015-12-08 23:52:43 +0800

    this is ex48 on drills deal with capitalize and another way to change str to int

---
+++ ex48/src/ex48.py
@@ -0,0 +1,43 @@
+import string
+
+
+class lexicon(object):
+    '''
+    @staticmethod
+    def conver_number(s):
+        try:
+            return int(s)
+        except ValueError:
+            return None
+    '''
+
+    @staticmethod
+    def conver_number(s):
+        try:
+            return string.atoi(s)
+        except ValueError:
+            return None
+
+    @staticmethod
+    def scan(sentence):
+        result = []
+        sen = sentence.lower()
+        words = sen.split()
+
+        while words:
+            if words[0] in ['east', 'west', 'north', 'south']:
+                result.append(('direction', words[0]))
+            elif words[0] in ['go', 'kill', 'stop', 'eat']:
+                result.append(('verb', words[0]))
+            elif words[0] in ['door', 'bear', 'princess', 'cabinet']:
+                result.append(('noun', words[0]))
+            elif words[0] in ['the', 'in', 'of', 'from', 'at']:
+                result.append(('stop', words[0]))
+            elif lexicon.conver_number(words[0]):
+                num = int(words[0])
+                result.append(('number', num))
+            else:
+                result.append(('error', words[0]))
+            words = words[1:]
+
+        return result
