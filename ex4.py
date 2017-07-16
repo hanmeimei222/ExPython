c916f5c1419c0fbf1a838bda8245ddc62be7dd64
hanmeimei <978288958@qq.com>
2017-07-16 16:57:48 +0800

    ex4

--- ex4.py
+++ ex4.py
@@ -15,3 +15,5 @@ print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
 print "We have", passengers, "to carpooll today."
 print "We need to put about", average_passengers_per_car, "in each car."
+
+
